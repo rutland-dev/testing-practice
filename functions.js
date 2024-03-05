08bbf66e7c884014d902d3fbefa05fe2cc9b5265
Joshua Rutland <144169110+rutland-dev@users.noreply.github.com>
2024-03-05 14:09:15 -0500

    Add caesarShift function and testing

--- functions.js
+++ functions.js
@@ -25,8 +25,51 @@ const calculator = {
   }
 }
 
+const regex = /[A-Za-z0-9!,.?:;"' ]/g
+
+const letters = (() => {
+  const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+  return caps.concat(caps.map(letter => letter.toLowerCase()));
+})();
+
+const numberSymbolArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", ",", ".", "?", " "];
+
+const cipherArray = numberSymbolArray.concat(letters);
+
+function testString(string) {
+  if (string.match(regex).length === string.length) return true;
+}
+
+function caesarCipher(string, shift) {
+  if (!testString(string)) {
+    throw new Error("invalid string");
+  }
+
+  let result = "";
+
+  for (let i = 0; i < string.length; i += 1) {
+    const index = cipherArray.indexOf(string[i]);
+
+    if (index + shift >= cipherArray.length) {
+      result += cipherArray[(index + shift) % cipherArray.length];
+    } else if ((index + shift) < 0) {
+      let n = 0;
+      let tempShift = shift;
+      while (index + tempShift < 0) {
+        n += 1;
+        tempShift += 1;
+      }
+      result += cipherArray[cipherArray.length - n];
+    } else {
+      result += cipherArray[index + shift];
+    }
+  }
+  return result;
+}
+
 module.exports = {
   capitalize: capitalize,
   reverseString: reverseString,
-  calculator: calculator
+  calculator: calculator,
+  caesarCipher: caesarCipher
 }
--- functions.test.js
+++ functions.test.js
@@ -27,3 +27,15 @@ test("divides 'a' by 'b'", () => {
 test("multiplies two numbers", () => {
   expect(functions.calculator.multiply(3, 2)).toEqual(6);
 })
+
+test("'testing! 123*' should throw Error", () => {
+  expect(() => functions.caesarCipher("testing! **", 5)).toThrow(Error);
+})
+
+test("'test! 123' should return 'O NOfjWXY' if using a shift of 36", () => {
+  expect(functions.caesarCipher("test! 123", 36)).toMatch(/^O NOfjWXY$/);
+})
+
+test("'O NOfjWXY' should return 'test! 123' if using a shift of -36", () => {
+  expect(functions.caesarCipher("O NOfjWXY", -36)).toMatch(/^test! 123$/);
+})
